6d55c18d3c5e59a4ffbf0acdf51850bdcbc831e3
Aparna Garg <gargaparna78@gmail.com>
2026-06-16 18:10:36 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.java
@@ -1,54 +1,44 @@
 class Solution {
-    
     public boolean isCycle(int V, int[][] edges) {
         // Code here
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         int n = edges.length;
         int m = edges[0].length;
-        boolean[]vis = new boolean[V];
-        for(int i = 0 ; i  < V ; i++){
+        int[] vis = new int[V];
+        for(int i = 0 ; i < V ; i++){
             adj.add(new ArrayList<>());
         }
-       for(int[] edge : edges){
-           int u = edge[0];
-           int v = edge[1];
-           adj.get(u).add(v);
-           adj.get(v).add(u);
-       }
-        for(int i = 0 ; i  <V ; i++)vis[i] = false;
+        // for(int i = 0 ; i < n ; i++){
+        //     for(int j = 0 ; j < m ; j++ ){
+        //         if(edges[i][j] == 1 && i != j){
+        //             adj.get(i).add(j);
+        //             adj.get(j).add(i);
+        //         }
+        //     }
+        // }
+        for(int[] edge : edges){
+            int v = edge[0];
+            int u = edge[1];
+            adj.get(v).add(u);
+            adj.get(u).add(v);
+        }
         for(int i = 0 ; i < V ; i++){
-            if(vis[i] == false){
-                if(detect(i,V,adj,vis) == true)return true;
+            if(vis[i] == 0){
+                if(dfs(i,-1,vis,adj) == true)return true;
             }
         }
         return false;
         
     }
-    boolean detect(int src ,int n , ArrayList<ArrayList<Integer>> adj , boolean vis[]){
-        vis[src] = true;
-        Queue<Pair>q = new LinkedList<>();
-        q.add(new Pair(src , -1));
-        while(!q.isEmpty()){
-            int node = q.peek().first;
-            int parent = q.peek().second;
-            q.poll();
-            for(int adjN : adj.get(node)){
-                if(vis[adjN] == false){
-                    vis[adjN] = true;
-                    q.add(new Pair(adjN , node));
-                }else if(parent != adjN){
-                    return true;
-                }
+    boolean dfs(int node , int parent , int vis[] , ArrayList<ArrayList<Integer>>adj){
+        vis[node] = 1;
+        for(int i : adj.get(node)){
+            if(vis[i] == 0){
+                if(dfs(i,node,vis,adj) == true)
+                return true;
             }
+            else if(i != parent) return true;
         }
         return false;
-    }
-}
-class Pair{
-    int first ;
-    int second;
-    Pair(int first , int second){
-        this.first = first;
-        this.second = second;
     }
 }
