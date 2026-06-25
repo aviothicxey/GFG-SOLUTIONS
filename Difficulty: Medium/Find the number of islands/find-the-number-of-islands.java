21c93a7006b1d2dc6f9dd1a4d254a6f60ad4f90d
Aparna Garg <gargaparna78@gmail.com>
2026-06-25 08:37:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find the number of islands/find-the-number-of-islands.java
+++ Difficulty: Medium/Find the number of islands/find-the-number-of-islands.java
@@ -1,68 +1,49 @@
-import java.util.*;
-
 class Solution {
     public int countIslands(char[][] grid) {
+        // Code here
         int n = grid.length;
         int m = grid[0].length;
-
-        int[][] vis = new int[n][m];
         int cnt = 0;
-
-        for(int row = 0; row < n; row++) {
-            for(int col = 0; col < m; col++) {
-
-                if(vis[row][col] == 0 && grid[row][col] == 'L') {
+        int[][] vis = new int[n][m];
+        for(int i = 0 ; i < n ; i++){
+            for(int j = 0 ; j < m ; j++){
+                if(grid[i][j] == 'L' && vis[i][j] == 0){
                     cnt++;
-                    bfs(row, col, vis, grid);
+                    bfs(i,j,vis , grid , n , m);
                 }
             }
         }
-
         return cnt;
     }
-
-    public void bfs(int ro, int co, int[][] vis, char[][] grid) {
-
-        vis[ro][co] = 1;
-
-        Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(ro, co));
-
-        int n = grid.length;
-        int m = grid[0].length;
-
-        while(!q.isEmpty()) {
-
-            int row = q.peek().first;
-            int col = q.peek().second;
+    public void bfs(int row , int col , int[][] vis , 
+    char[][]grid , int n , int m){
+        vis[row][col] = 1;
+        Queue<Pair> q= new LinkedList<>();
+        
+        q.add(new Pair(row,col));
+        while(!q.isEmpty()){
+            int r = q.peek().row;
+            int c = q.peek().col;
             q.poll();
-
-            for(int drow = -1; drow <= 1; drow++) {
-                for(int dcol = -1; dcol <= 1; dcol++) {
-
-                    int nrow = row + drow;
-                    int ncol = col + dcol;
-
-                    if(nrow >= 0 && ncol >= 0 &&
-                       nrow < n && ncol < m &&
-                       grid[nrow][ncol] == 'L' &&
-                       vis[nrow][ncol] == 0) {
-
+            for(int drow = -1 ;  drow <= 1 ; drow++){
+                for(int dcol = -1 ; dcol <=1 ; dcol++){
+                    int nrow = r + drow;
+                    int ncol = c + dcol;
+                    if(nrow >= 0 && ncol >= 0 && nrow < n && ncol < m 
+                    && grid[nrow][ncol] == 'L' && vis[nrow][ncol] == 0){
                         vis[nrow][ncol] = 1;
-                        q.add(new Pair(nrow, ncol));
+                        q.add(new Pair(nrow,ncol));
                     }
                 }
             }
         }
     }
 }
-
-class Pair {
-    int first;
-    int second;
-
-    Pair(int first, int second) {
-        this.first = first;
-        this.second = second;
+class Pair{
+    int row;
+    int col;
+    Pair(int row, int col){
+        this.row = row;
+        this.col = col;
     }
 }
